79b72eda6edcc7f68fcdfc86096d74594534d1e8
Andrey Fedinyak <fedinyakandrey@gmail.com>
2025-07-17 15:14:59 +0300

    Removed useless comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-// import { useState } from "react";
 import { useState } from "react";
-
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-// import Form from "./components/FormOld";
 import Cube from "./components/Cube/Cube";
-// import Cat from "./components/Cat";
 import Form from "./components/Form/Form";
 import Score from "./components/Score/Score";
 import LanguageSwitchBtn from "./components/Ui/LanguageSwitchBtn";
 
 function App() {
-  // const [count, setCount] = useState(0);
   const [score, setScore] = useState({ player: 0, cat: 0 });
   const [isLogin, setIsLogin] = useState(false);
   const [isCatPushButton, setIsCatPushButton] = useState(false);
@@ -45,7 +37,6 @@ function App() {
   const catPushButton = () => {
     setIsLogin(false);
     setIsCatPushButton(true);
-    // console.log(score, "score");
   };
 
   const catGetScore = () => {
@@ -72,30 +63,8 @@ function App() {
         >
           <Form loginHandle={loginHandle} />
           <LanguageSwitchBtn />
-
-          {/* <Cat setIsLogin={setIsLogin} setIsCatPushButton={setIsCatPushButton} /> */}
         </Cube>
       </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      {/* <h1>Vite + React 1234567890</h1> */}
-      {/* <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
